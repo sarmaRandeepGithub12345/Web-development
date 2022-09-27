01d968ebc990cd43802537d20b2791c425af5ce9
Randeep Sarma <92437704+randeepsarma@users.noreply.github.com>
2022-09-27 19:11:01 +0530

    Add files via upload

---
+++ nodejs/import/index.mjs
@@ -0,0 +1,8 @@
+import path from 'path';
+
+console.log(path.basename('c:\\nodejs\\import\\index.js'));
+// console.log(__filename);   These are invalid as these are not defined in the ES module scope
+// console.log(__dirname);   These are invalid
+
+
+
